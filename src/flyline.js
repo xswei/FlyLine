321593d29dcacb165e916fdedbaf6f13aa220c76
xs.wei@foxmail.com <xs.wei@foxmail.com>
2017-03-04 19:00:06 +0800

    add files

--- src/flyline.js
+++ src/flyline.js
@@ -3,48 +3,125 @@
 	typeof define === 'function' && define.amd ? define(['exports'],factory):
 	(factory((global.flyline = global.flyline || {})));
 }(this,(function(exports){
+	var flys = [],
+		canvas = null,
+		context = null,
+		that = exports;
 
-	var that = exports;
+
+	var stats = new Stats();
+
+	document.querySelector("#stats").appendChild(stats.domElement);
 	
-	function Fly(o){
-		this.s = o.s;
-		this.e = o.e;
-		this.t = o.t?o.t:1000;
-		that.flys.push(this);
-		return that;
+	function addFlyQ(o){
+		var f = {
+			s:{},
+			e:{},
+			tt:0,
+			t:o.t?o.t:1000,
+			color:'#'+Math.floor(Math.random()*16777215).toString(16)
+		};
+		try{
+			f.s.x = o.start.x;
+			f.s.y = o.start.y;
+			f.e.x = o.end.x;
+			f.e.y = o.end.y;
+		}catch(e){
+			console.log("aaa")
+			throw new Error("Fail to add Fly,please check parameter format!");
+			return ;
+		}
+		f.c = (o.c && o.c.x && o.c.y)?{x:o.c.x,y:o.c.y}:{x:f.s.x,y:f.s.y};
+		f.step = 0.005*Math.random();
+		flys.push(f);
+		if(flys.length==1){
+			render();
+		}
 	}
 
+
+
+
+
+	function addFlyC(){
+
+	}
 	function setCanvas(s){
-		var canvas;
 		if(typeof s === "string"){
 			canvas = document.querySelector(s);
-		}else{
+			context = canvas.getContext('2d');
+			context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+			context.fillRect(0, 0, canvas.width, canvas.height);
+		}else if(s instanceof HTMLCanvasElement){
 			canvas = s;
-		}
-		if(s instanceof HTMLCanvasElement){
-			this.domElement = s;
-			this.
+			context = canvas.getContext('2d');
+			context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+			context.fillRect(0, 0, canvas.width, canvas.height);
 		}else{
-			throw new Error("Fail to init Canvas Element!");
+			throw new Error("Fail to set Canvas!");
 		}
+	}
+
+	function render(){
+		context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		drawFly();
+		stats.update();
+		requestAnimationFrame(render);
+	}
+	function drawFly(){
+		if(!(canvas&&context)){
+			return ;
+		}
+		var newA = [];
+		for(var i=0;i<flys.length;++i){
+			flys[i].tt += flys[i].step;
+			if(flys[i].tt<1){
+				newA.push(flys[i]);
+			}
+		}
+		/*flys = flys.filter(function(d){
+			d.tt += d.step;
+			return d.tt<1;
+		})*/
+		flys = newA;
+		flys.forEach(function(d){
+			var np = getPointQuadRaticBeizer(d.s,d.c,d.e,d.tt);
+			context.beginPath();
+			context.fillStyle = d.color;
+			context.arc(np.x,np.y,3,0,Math.PI*2,false);
+			context.fill();
+		})
 	}
 
 
 
-	function getPointInQuadRaticBeizer(s,c,e,t){
+
+
+	function getPointQuadRaticBeizer(s,c,e,t){
 		var x = Math.pow(1-t,2) * s.x + 2 * (1-t) * t * c.x + Math.pow(t,2) * e.x; 
     	var y = Math.pow(1-t,2) * s.y + 2 * (1-t) * t * c.y + Math.pow(t,2) * e.y; 
    		return {x:x,y:y};
 	}
-	
+	function getPointCubicBezier(s,c1,c2,e,t){
+		var x=CubicN(t,s.x,c1.x,c2.x,e.x);
+    	var y=CubicN(t,s.y,c1.y,c2.y,e.y);
+    	return({x:x,y:y});
+	}
+	function CubicN(pct, a,b,c,d) {
+	    var t2 = pct * pct;
+	    var t3 = t2 * pct;
+	    return a + (-a * 3 + pct * (3 * a - a * pct)) * pct
+	    + (3 * b + pct * (-6 * b + b * 3 * pct)) * pct
+	    + (c * 3 - c * 3 * pct) * t2
+	    + d * t3;
+	}
 
 
 
 
 
 	exports.version = '1.0.0';
-	exports.flys = [];
-	exports.Fly = Fly;
+	exports.addFlyQ = addFlyQ;
 	exports.setCanvas = setCanvas;
-
 })));
